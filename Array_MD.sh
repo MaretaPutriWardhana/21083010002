48a480a16399edee0a8acb2e3722b945ccde7cac
Mareta Putri Wardhana <putri.mareta@gmail.com>
2022-10-12 13:53:22 +0700

    Tugas6

--- Array_MD.sh
+++ Array_MD.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# deklarasi array2dimensi " : " pemisah nilai (array [3][4])
+#deklarasi array2dimensi " : " pemisah nilai (array [3][4])
 array2dimensi="1.1:1.2:1.3:1.4 2.1:2.2:2.3:2.4 3.1:3.2:3.3:3.4"
 
-# mengakali multi dimensi -> dengan pemisah dimensi "tr :"
+#mengakali multi dimensi -> dengan pemisah dimensi "tr :"
 function dimensiBaris {
     for baris in $array2dimensi
     do
